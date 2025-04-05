d5b08c0c092865f2618ef3377c98a122750b6812
nailarivero <nailariveroo@gmail.com>
2025-04-05 01:38:44 -0300

    Update database.js

--- miniApp/database.js
+++ miniApp/database.js
@@ -1,6 +1,9 @@
-// BASE DE DATOS (Simulada)
+// simulacion de la bd
 const tareasGuardadas = [];
 
 function guardarTarea(tarea) {
   tareasGuardadas.push(tarea);
 }
+  // Escalabilidad: en un futuro se podría guardar en una base de datos real.
+  // Mantenibilidad: se puede reemplazar esta función sin tocar backend o frontend.
+  // Seguridad: esta capa no expone directamente los datos al usuario.
