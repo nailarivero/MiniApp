a314406b09e6e4092133b49cea94eb7ffdb739dc
nailarivero <nailariveroo@gmail.com>
2025-04-05 01:37:19 -0300

    Update backend.js

--- miniApp/backend.js
+++ miniApp/backend.js
@@ -1,10 +1,10 @@
-// BACKEND
+// recibe una funcion, la valida y responde un mensaje
 function backendRecibirTarea(tarea) {
     if (tarea.trim() === "") {
-      return "Error: La tarea no puede estar vacía.";
+      return "Error: La tarea no puede estar vacía.";   // Seguridad: se valida el input (no vacío) antes de guardarlo.
     }
-  
-    guardarTarea(tarea); // Llama al "módulo" de base de datos
-    return `Tarea guardada: ${tarea}`;
+   // Mantenibilidad: la validación y la lógica están separadas del frontend.
+    guardarTarea(tarea); // llama al "módulo" de base de datos
+    return `Tarea guardada: ${tarea}`;  
   }
-  
+// Escalabilidad: se podría conectar a una api
