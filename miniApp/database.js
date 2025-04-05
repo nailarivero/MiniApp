c19f3e73fdabab93c66722c1a8eec619490962b6
nailarivero <nailariveroo@gmail.com>
2025-04-05 01:44:50 -0300

    Update database.js

--- miniApp/database.js
+++ miniApp/database.js
@@ -1,8 +1,8 @@
 // simulacion de la bd
-const tareasGuardadas = [];
+const tareasGuardadas = []; //crea la lista donde se van a guardar las tareas
 
-function guardarTarea(tarea) {
-  tareasGuardadas.push(tarea);
+function guardarTarea(tarea) { //declara la fucnion que va a recibir la tarea
+  tareasGuardadas.push(tarea); //agrega la tarea 
 }
   // Escalabilidad: en un futuro se podría guardar en una base de datos real.
   // Mantenibilidad: se puede reemplazar esta función sin tocar backend o frontend.
